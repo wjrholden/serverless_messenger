8183daab295e0a9a1cd68ffdc769ea396b6753ae
William Holden <wjrholden@gmail.com>
2020-02-16 10:24:48 -0600

    initial commit

---
+++ messenger/handler.py
@@ -0,0 +1,30 @@
+import json
+
+
+def hello(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
+    """
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
+
+# You can setup your AWS account later. More details available here:
+#   http://slss.io/aws-creds-setup
+
+if __name__ == "__main__":
+    hello(None, None)
